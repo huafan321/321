330da699f39ec28378e442c11cef0e194338f4d6
onceocean321@hotmail.com <onceocean321@hotmail.com>
2025-12-23 16:43:20 +0800

    finished lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -10,6 +12,7 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private static final int INFINITY = Integer.MAX_VALUE;
     public MazeCycles(Maze m) {
         super(m);
     }
@@ -17,8 +20,35 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        Stack<Integer> dfs = new Stack<>();
+        for(int v = 0; v < maze.V(); v++){
+            edgeTo[v] = INFINITY;
+            distTo[v] = INFINITY;
+        }
+        distTo[0] = 0;
+        edgeTo[0] = 0;
+        dfs.push(0);
+
+        while(!dfs.isEmpty()){
+           int x = dfs.pop();
+           marked[x] = true;
+           for(int n : maze.adj(x)){
+               if (marked[n] && !isParent(n,x)){
+                   announce();
+                   return;
+               }
+               if (!marked[n]){
+                   edgeTo[n] = x;
+                   distTo[n] = distTo[x] + 1;
+                   dfs.push(n);
+               }
+           }
+        }
     }
 
     // Helper methods go here
+    private boolean isParent(int a, int b){
+        return edgeTo[b] == a;
+    }
 }
 
